4a2aa2a3db32bff43c1781ba5fcb0547445deee6
Akash Jaiswal <akashjais929@gmail.com>
2025-01-23 01:08:25 +0530

    Improved dependency management and routing

--- src/main/java/com/moviex/api/gateway/ApiGatewayApplication.java
+++ src/main/java/com/moviex/api/gateway/ApiGatewayApplication.java
@@ -2,6 +2,8 @@ package com.moviex.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
--- src/main/java/com/moviex/api/gateway/controllers/DemoController.java
+++ src/main/java/com/moviex/api/gateway/controllers/DemoController.java
@@ -1,22 +1,24 @@
 package com.moviex.api.gateway.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class DemoController {
 
-    @Value("${jwt.secret}")
-    private String secretKey;
+//    @Value("${jwt.secret}")
+//    private String secretKey;
 
     @Value("${spring.boot.message}")
     private String message;
 
     @GetMapping("/")
     public ResponseEntity<Void> index() {
-        System.out.println(message + " " + secretKey);
+        System.out.println(message);
         return ResponseEntity.ok().build();
     }
 }
